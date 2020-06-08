074173392d6e897d0f4573be5907b665d1a8b83d
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-06-08 15:37:29 +0000

    created a server w/express

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 8080; // Default PORT 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouse.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on ${PORT}!`);
+});
