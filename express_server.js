45421c334116f7252157f6ba44b7749d132d1192
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-06-08 15:44:00 +0000

    +1 path handler

--- express_server.js
+++ express_server.js
@@ -10,8 +10,13 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
 
 app.listen(PORT, () => {
