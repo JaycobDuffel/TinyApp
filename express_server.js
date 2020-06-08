adc9d3b27f89b618e927e52402fd0f345bd5e1ca
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-06-08 15:54:56 +0000

    express_server.js w/view-engine

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // Default PORT 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouse.ca",
